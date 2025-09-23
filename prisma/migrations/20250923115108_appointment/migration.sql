b71432de49dc1e6035c505f7be239f9533699189
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2025-09-23 12:53:19 +0100

    appointment

---
+++ prisma/migrations/20250923115108_appointment/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Appointment" ALTER COLUMN "startTime" SET DATA TYPE TEXT,
+ALTER COLUMN "endTime" SET DATA TYPE TEXT;
--- src/features/appointment/presentation/dto/appointmentDto.js
+++ src/features/appointment/presentation/dto/appointmentDto.js
@@ -20,14 +20,24 @@ class AppointmentDto {
             service: Joi.array().items(Joi.object()),
             date: Joi.date().required(),
             isRecurring: Joi.boolean().default(false),
-            startTime: Joi.date().required(),
-            endTime: Joi.date().required(),
+            startTime: Joi.string()
+                .pattern(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/)
+                .required()
+                .messages({
+                    "string.pattern.base": "startTime must be in HH:mm or HH:mm:ss format",
+                }),
+
+            endTime: Joi.string()
+                .pattern(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/)
+                .required()
+                .messages({
+                    "string.pattern.base": "endTime must be in HH:mm or HH:mm:ss format",
+                }),
             recurrence: Joi.object().default({}),
             isBillable: Joi.boolean().default(true),
             serviceLocation: Joi.string().min(1).required(),
             requiresTravel: Joi.boolean().default(false),
             colourCode: Joi.string().min(1).required(),
-            relatedAppointment: Joi.string().uuid().optional()
         });
 
         Validator.validateRequest(req, next, schema);
@@ -42,8 +52,19 @@ class AppointmentDto {
             service: Joi.array().items(Joi.object()),
             date: Joi.date(),
             isRecurring: Joi.boolean(),
-            startTime: Joi.date(),
-            endTime: Joi.date(),
+            startTime: Joi.string()
+                .pattern(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/)
+                .required()
+                .messages({
+                    "string.pattern.base": "startTime must be in HH:mm or HH:mm:ss format",
+                }),
+
+            endTime: Joi.string()
+                .pattern(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/)
+                .required()
+                .messages({
+                    "string.pattern.base": "endTime must be in HH:mm or HH:mm:ss format",
+                }),
             recurrence: Joi.object(),
             isBillable: Joi.boolean(),
             serviceLocation: Joi.string().min(1),
