2208528838f3c79b99afd888ff04384b45078627
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2025-09-26 23:17:18 +0100

    appointment

--- src/features/appointment/application/appointmentService.js
+++ src/features/appointment/application/appointmentService.js
@@ -32,7 +32,7 @@ class AppointmentService {
 
     async updateAppointment(data) {
         const appointment = await this.appointmentRepository.findOne({ id: data.id });
-        if (!data.forAll && data.relatedAppointment) {
+        if (!data.forAll && data.relatedAppointment || !appointment.isRecurring) {
             if (appointment && appointment.relatedAppointment) {
                 const update = await this.appointmentRepository.update(data.id, {
                     clientId: data.clientId || appointment.clientId,
