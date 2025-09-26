752b1fd579364c9cafb0f712bb24816a9ad5592d
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2025-09-26 17:42:13 +0100

    appointment

--- src/features/appointment/application/appointmentService.js
+++ src/features/appointment/application/appointmentService.js
@@ -62,7 +62,7 @@ class AppointmentService {
 
                 return update;
             } else {
-                const appointmentData = new Appointment({ ...appointment, ...data });
+                const appointmentData = new Appointment({ ...data, ...appointment });
                 const newAppointment = await this.appointmentRepository.create(appointmentData.createAppointment);
 
                 if (!newAppointment) {
