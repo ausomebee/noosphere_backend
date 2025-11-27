e3709e7a85d71eabdd68dd63e1646f691b95a44a
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2025-11-27 16:49:27 +0100

    client changes

--- src/features/pipeline/presentation/controllers/pipeline.Controller.js
+++ src/features/pipeline/presentation/controllers/pipeline.Controller.js
@@ -192,7 +192,6 @@ class PipelineController {
 
     getItemByIdClient = expressAsyncHandler(async (req, res) => {
         const item = await this.service.getItemByIdClient(req.params.id);
-
         if (!item) {
             res.status(500).json({ message: 'Failed to fetch item' });
         }
