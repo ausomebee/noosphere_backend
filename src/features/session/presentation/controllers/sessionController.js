2d02f18d56e6e22d0e9db596f5a1a0f0307fb2e1
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2025-12-09 21:57:32 +0100

    seession apis

--- src/features/session/presentation/controllers/sessionController.js
+++ src/features/session/presentation/controllers/sessionController.js
@@ -110,14 +110,13 @@ class SessionController {
 
             return {
                 id: s.id,
-                clientName:
-                    s.appointment.client.preferredName ||
-                    `${s.appointment.client.firstName} ${s.appointment.client.lastName}`,
+                clientName: `${s.appointment.client.firstName} ${s.appointment.client.lastName}`,
                 sessionTypeName: s.appointment.session.name,
                 clinician: s.appointment.clinicians?.map(c => c.fullName).join(", "),
                 clientApprovalStatus: s.clientApprovalStatus,
                 supervisorApprovalStatus: s.supervisorApprovalStatus,
                 totalHours,
+                date: s.createdAt
             };
         });
 
