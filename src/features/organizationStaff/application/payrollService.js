8047d25dfe1a1e6cab89a2be193b1ffa5e8370c4
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-02-21 00:24:29 +0100

    teams

--- src/features/organizationStaff/application/payrollService.js
+++ src/features/organizationStaff/application/payrollService.js
@@ -15,7 +15,7 @@ class PayrollService {
             ratePerHour: data.ratePerHour || payroll.ratePerHour,
             minimumHours: data.minimumHours || payroll.minimumHours,
             incomeItems: {
-                set: data.incomeItems || payroll.incomeItems
+                set: data.otherPays || payroll.incomeItems
             },
             deductions: {
                 set: data.deductions || payroll.deductions
