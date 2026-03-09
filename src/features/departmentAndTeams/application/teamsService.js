dc30a68948292ca1967355e32582e4f49d93c960
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-03-09 07:49:37 +0100

    payment link

--- src/features/departmentAndTeams/application/teamsService.js
+++ src/features/departmentAndTeams/application/teamsService.js
@@ -62,8 +62,8 @@ class TeamsService {
         return await this.teamsRepository.findAllAndPopulate(query, {
             teamLead: {
                 select: {
-                    firstName: true,
-                    lastName: true,
+                    id: true,
+                    fullName: true,
                 }
             },
             teamMembers: {
