c9cd56e2aaa396591031dc7f5c48bfbe7811fbde
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-03-18 00:57:09 +0100

    pipeline docs

--- src/features/customForms/application/FormResponseService.js
+++ src/features/customForms/application/FormResponseService.js
@@ -43,7 +43,7 @@ class FormResponseService {
     }
 
     async getFormResponses(formId) {
-        const responses = await this.formResponseRepository.findAllAndPopulate({ formId }, { fields: true, form: true });
+        const responses = await this.formResponseRepository.findAllAndPopulate({ formId }, { fields: { include: { formField: true } } });
 
         if (!responses || responses.length === 0) {
             throw new Error("No Form Responses found");
