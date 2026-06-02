42c6ee0272f79a2a6861910a45e88120f997acb5
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-06-02 21:44:31 +0100

    test cicd

--- src/features/role/presentation/routes/roleRoute.js
+++ src/features/role/presentation/routes/roleRoute.js
@@ -177,7 +177,7 @@ class RoleRoutes {
          *       400:
          *         description: Validation error
          */
-        this.router.post("/", ()=>{console.log("e reach")}, adminProtect(), RoleDto.createRoleDto, this.controller.createRole);
+        this.router.post("/", adminProtect(), ()=>{console.log("e reach")}, RoleDto.createRoleDto, this.controller.createRole);
 
         /**
          * @swagger
