bc0eed57c878ad6e35c33cec2f591f36f8b42d03
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-06-02 22:31:48 +0100

    test cicd

--- src/features/role/presentation/controllers/roleController.js
+++ src/features/role/presentation/controllers/roleController.js
@@ -106,7 +106,7 @@ class RoleController {
     });
 
     createRole = expressAsyncHandler(async (req, res) => {
-        console.log("Request body for creating role:", req.body);
+        console.log("Request body for creating role:", JSON.stringify(req.body));
         const roleData = new Role(req.body);
         const role = await this.service.createRole(roleData.createRole);
 
