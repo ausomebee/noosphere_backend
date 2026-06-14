907f58d9e539274f345a52be98bd885e48b580da
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-06-14 02:18:05 +0100

    tenant admin choices update endpoint

--- src/features/pipeline/presentation/routes/pipelineRoute.js
+++ src/features/pipeline/presentation/routes/pipelineRoute.js
@@ -696,7 +696,7 @@ class PipelineRoutes {
 
         /**
          * @swagger
-         * /api/v1/pipeline/overview/{tenantId}:
+         * /api/v1/pipeline/tenant/overview/{tenantId}:
          *   get:
          *     summary: Get pipeline overview
          *     tags: [Pipeline]
@@ -714,7 +714,7 @@ class PipelineRoutes {
          *         description: Validation error
          */
 
-        this.router.get("/overview/:tenantId", staffProtect(), this.controller.getTenantPipelineSummary);
+        this.router.get("/tenant/overview/:tenantId", staffProtect(), this.controller.getTenantPipelineSummary);
 
         /**
          * @swagger
