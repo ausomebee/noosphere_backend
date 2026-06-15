6017792f12a332973e3b5fba77e12c9a1d2798a9
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-06-15 14:11:03 +0100

    tenant admin choices update endpoint

--- src/features/tenant/presentation/routes/tenantRoute.js
+++ src/features/tenant/presentation/routes/tenantRoute.js
@@ -1072,6 +1072,28 @@ class TenantRoutes {
 
         /**
          * @swagger
+         * /api/v1/tenant/tenant/avg-staff/{tenantId}:
+         *   get:
+         *     summary: Count average client per therapist
+         *     tags: [Tenant]
+         *     parameters:
+         *       - in: path
+         *         name: tenantId
+         *         required: true
+         *         schema:
+         *           type: string
+         *           format: uuid
+         *         description: The ID of the tenant
+         *     responses:
+         *       200:
+         *         description: Staffs counted successfully
+         *       400:
+         *         description: Bad request
+         */
+        this.router.get("/tenant/avg-staff/:tenantId", adminProtect(), this.controller.averageClinicians);
+
+        /**
+         * @swagger
          * /api/v1/tenant/forgotpassword/{email}:
          *   get:
          *     summary: send forgot password mail
