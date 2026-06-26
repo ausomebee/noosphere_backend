cd927894a1b5a49833795af598e7cefc07efdfcf
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-06-26 11:37:03 +0100

    tenant admin choices update endpoint

---
+++ prisma/migrations/20260626103613_init/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Tenant_email_key";
+
+-- DropIndex
+DROP INDEX "Tenant_phoneNumber_key";
+
+-- DropIndex
+DROP INDEX "Tenant_subdomain_key";
+
+-- DropIndex
+DROP INDEX "TenantStaff_email_key";
