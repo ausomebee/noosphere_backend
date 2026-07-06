40f1f00168d7a571a42dfa14588db7be57f65442
Abdulmalik Amzat <ayodejiamzat@gmail.com>
2026-07-06 18:56:56 +0100

    com

--- src/features/messaging/presentation/routes/messageRoutes.js
+++ src/features/messaging/presentation/routes/messageRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import MessageController from "../controllers/messageController.js";
 import MessageDto from "../dtos/messageDto.js";
-import { staffProtect } from "../../../../middleware/auth_handlers.js";
+import { clientProtect, staffProtect } from "../../../../middleware/auth_handlers.js";
 
 /**
  * @swagger
